9244c4c6e225e8a3caa578415c347130d5cddc32
agustinchavez <chavez13@illinois.edu>
2017-08-09 14:39:28 -0500

    fixed showing of taggings

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,9 +1,19 @@
 class TagsController < ApplicationController
+  before_action :authenticate_user!
 
   def index
+    @tags = storage.list_for(params[:page], params[:tag])
   end
 
   def show
     @tag = Tag.find(params[:id])
   end
+
+  private
+
+  def storage
+    Tag
+  end
+
+
 end
