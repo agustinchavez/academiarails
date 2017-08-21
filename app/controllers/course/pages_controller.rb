53e7b96f2064c77aaba6174554f8d372ab2804a0
agustinchavez <chavez13@illinois.edu>
2017-08-21 13:58:33 -0500

    revert changes

--- app/controllers/course/pages_controller.rb
+++ app/controllers/course/pages_controller.rb
@@ -12,7 +12,7 @@ module Course
       @email = params[:email]
       @message = params[:message]
       ActionMailer::Base.mail(:from => @email,
-          :to => 'chavez.agustin@outlook.com',
+          :to => 'academiarails@gmail.com',
           :subject => "A new contact form message from #{@name}",
           :body => @message).deliver_now
     end
